1c10a0f5db1fdbcdfa2ad5923434e24002afb3b5
ediaz13 <diazemanuel27@gmail.com>
2020-04-21 23:41:31 -0300

    AlphaVantageScript

--- 2 - YahooStocks.py
+++ 2 - YahooStocks.py
@@ -1,8 +1,7 @@
 # ============================================================================
 # Import OHLCV data using yahoofinancials
-# Author - Mayank Rasu
+# Author - Emanuel DIaz
 # =============================================================================
-
 
 import pandas as pd
 from yahoofinancials import YahooFinancials
@@ -13,11 +12,11 @@ all_tickers = ["AAPL","MSFT","CSCO","AMZN","INTC"]
 # extracting stock data (historical close price) for the stocks identified
 close_prices = pd.DataFrame()
 end_date = (datetime.date.today()).strftime('%Y-%m-%d')
-beg_date = (datetime.date.today()-datetime.timedelta(1825)).strftime('%Y-%m-%d')
+beg_date = (datetime.date.today()-datetime.timedelta(365)).strftime('%Y-%m-%d')
 cp_tickers = all_tickers
 attempt = 0
 drop = []
-while len(cp_tickers) != 0 and attempt <=5:
+while len(cp_tickers) != 0 and attempt <=10:
     print("-----------------")
     print("attempt number ",attempt)
     print("-----------------")
@@ -25,7 +24,7 @@ while len(cp_tickers) != 0 and attempt <=5:
     for i in range(len(cp_tickers)):
         try:
             yahoo_financials = YahooFinancials(cp_tickers[i])
-            json_obj = yahoo_financials.get_historical_stock_data(beg_date,end_date,"daily")
+            json_obj = yahoo_financials.get_historical_price_data(beg_date,end_date,"daily")
             ohlv = json_obj[cp_tickers[i]]['prices']
             temp = pd.DataFrame(ohlv)[["formatted_date","adjclose"]]
             temp.set_index("formatted_date",inplace=True)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# Algorithmic Trading & Quantitative Analysis Using Python
+ 
 # udemy-stocks
+
+WTF
+It seems that you are already a registered user. As a reminder, your API key is: XXXXXXXXXXXXX. Thank you!
